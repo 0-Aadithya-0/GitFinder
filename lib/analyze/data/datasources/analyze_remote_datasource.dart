cdd3e38de372b15c0074b778059d11d3535fbb1d
aashika482 <aashiika1313@gmail.com>
2026-03-08 12:50:00 +0530

    Added data layer for analyze

---
+++ lib/analyze/data/datasources/analyze_remote_datasource.dart
@@ -0,0 +1,31 @@
+import 'package:dio/dio.dart';
+import '../../domain/entities/repo_result_entity.dart';
+import '../models/repo_result_model.dart';
+
+class AnalyzeRemoteDatasource {
+  final Dio _dio;
+
+  const AnalyzeRemoteDatasource(this._dio);
+
+  Future<List<RepoResultEntity>> analyzeQuery(String query) async {
+    final response = await _dio.post<List<dynamic>>(
+      '/analyze',
+      data: {'query': query},
+    );
+    return _parseResponse(response.data!);
+  }
+
+  Future<List<RepoResultEntity>> analyzeRepos(List<String> repoUrls) async {
+    final response = await _dio.post<List<dynamic>>(
+      '/analyze',
+      data: {'repos': repoUrls},
+    );
+    return _parseResponse(response.data!);
+  }
+
+  List<RepoResultEntity> _parseResponse(List<dynamic> data) {
+    return data
+        .map((e) => RepoResultModel.fromJson(e as Map<String, dynamic>))
+        .toList();
+  }
+}
